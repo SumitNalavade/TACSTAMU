fc63c4aeb58239bb4f7f8f7ec41a8d7c84ce2541
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-03-27 19:26:10 -0500

    Add build 4 good page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,6 +23,7 @@ const Navbar:React.FC<Props> = ({ selectedPage }) => {
                         <Link href="/join" className={`text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium ${selectedPage === "Join" || selectedPage === "join" ? 'text-primary border-b-2 border-primary font-bold font-xl' : ''}`}>Join</Link>
                         <Link href="/about" className={`text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium ${selectedPage === "About" || selectedPage === "about" ? 'text-primary border-b-2 border-primary font-bold font-xl' : ''}`}>About</Link>
                         <Link href="/events" className={`text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium ${selectedPage === "Events" || selectedPage === "events" ? 'text-primary border-b-2 border-primary font-bold font-xl' : ''}`}>Events</Link>
+                        <Link href="/b4g" className={`text-gray-500 hover:text-gray-700 px-3 py-2 text-sm font-medium ${selectedPage === "b4g" || selectedPage === "b4g" ? 'text-primary border-b-2 border-primary font-bold font-xl' : ''}`}>Build 4 Good</Link>
                         <a href="https://discord.gg/CBWn8mKFvx" target='_blank' className="flex items-center gap-x-2 bg-white border-2 border-b-4 border-r-4 border-primary text-primary px-4 py-2 rounded-full text-sm font-medium hover:bg-primary-light hover:text-white">
                             <FaDiscord className='text-xl mt-0.5' />
                             <p>Join our Discord</p>
